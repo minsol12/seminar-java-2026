05b4c779e730edfc7ec1d4fcf71bdb8290762f82
minsol12 <moonlisn04@naver.com>
2026-07-01 16:40:33 +0900

    UserManager 기본 구조

---
+++ src/main/java/com/example/java_seminar/schedule/User.java
@@ -0,0 +1,94 @@
+package com.example.java_seminar.schedule;
+
+import java.time.LocalDateTime;
+
+public class User {
+  private final int id;
+  private String name;
+  private String email;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+
+  private static int idCounter = 1;
+
+  public User() {
+    this.id = idCounter++;
+    LocalDateTime now = LocalDateTime.now();
+    this.createdAt = now;
+    this.updatedAt = now;
+  }
+
+  // 생성자에서 바로 setter 쓰기
+  public User(String name, String email) {
+    this();
+    setName(name);
+    setEmail(email);
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setName(String name) {
+    // null 금지, 공백만 있는 문자열 금지
+    if (name == null || name.isBlank()) {  // isBlank() - 문자열이 비어 있거나, 빈 공백으로만 이루어져 있으면, true를 리턴
+      throw new IllegalArgumentException("사용자 이름은 비어 있을 수 없슨..");
+    }
+
+    this.name = name;
+//    this.updatedAt = LocalDateTime.now();
+    touchUpdatedAt();
+  }
+
+  public void setEmail(String email) {
+    // null 금지, 공백 금지, @ 포함
+    if (email == null || email.isBlank()) {
+      throw new IllegalArgumentException("이메일은 비어 있을 수 없슨..");
+    }
+
+    if (!isValidEmail(email)) {
+      throw new IllegalArgumentException("올바른 이메일 형식이 아닙니다..");
+    }
+
+    this.email = email;
+//    this.updatedAt = LocalDateTime.now();
+    touchUpdatedAt();
+  }
+
+  // 이메일 검증 메서드
+  private boolean isValidEmail(String email) {
+    String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    return email.matches(emailRegex);
+  }
+
+  // touchUpdatedAt()
+  private void touchUpdatedAt() {
+    this.updatedAt = LocalDateTime.now();
+  }
+
+  // 사용자 목록 조회, 상세 조회 - 출력 메서드 - displayInfo()
+  public void displayInfo() {
+    System.out.println("=== 사용자 정보 ===");
+    System.out.println("id: " + id);
+    System.out.println("name: " + name);
+    System.out.println("email: " + email);
+    System.out.println("createdAt: " + createdAt);
+    System.out.println("updatedAt: " + updatedAt);
+  }
+}
---
+++ src/main/java/com/example/java_seminar/schedule/UserManager.java
@@ -0,0 +1,97 @@
+package com.example.java_seminar.schedule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserManager {
+  private List<User> users = new ArrayList<>();
+
+//- addUser
+//  같은 이메일 가진 사용자 중복 등록 불가
+//  중복이면 예외 던짐
+  public void addUser(User user) throws ScheduleException {
+    if (isDuplicateEmail(user.getEmail())) {
+      throw new ScheduleException("이미 사용 중인 이메일입니다.");
+    }
+
+    users.add(user);
+  }
+
+  // isDuplicateEmail
+  // 중복 검사 메서드
+  private boolean isDuplicateEmail(String email) {
+    for (User user : users) {
+      if (user.getEmail().equals(email)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  //- displayAllUsers
+  public void displayAllUsers() {
+    if (users.isEmpty()) {
+      System.out.println("등록된 사용자가 없습니다..");
+      return;
+    }
+
+    for (User user : users) {
+      user.displayInfo();
+      System.out.println();
+    }
+  }
+
+  // id 찾기
+  public User findById(int id) throws ScheduleException {
+    for (User user : users) {
+      if (user.getId() == id) {
+        return user;
+      }
+    }
+
+    throw new ScheduleException("해당 id의 사용자가 존재하지 않습니다..");
+  }
+
+//- displayUserById
+  public void displayUserById(int id) throws ScheduleException {
+    // findById() -> displayInfo();
+    User user = findById(id);
+    user.displayInfo();
+  }
+
+//- updateUser
+  // id 입력 받아
+  // name, email 수정
+  // 사용자 존재 여부
+  // 바꾸려는 이메일이 다른 사용자와 중복되는지 - 만들어야댐. 내 이메일 그대로 다시 입력하는 건 허용해야 하기 때문에
+  public void updateUser(int id, String name, String email) throws ScheduleException {
+    User user = findById(id);
+
+    if (isDuplicateEmailExceptUser(email, id)) {
+      throw new ScheduleException("이미 사용 중인 이메일입니다.");
+    }
+
+    user.setName(name);
+    user.setEmail(email);
+  }
+
+  private boolean isDuplicateEmailExceptUser(String email, int userId) {
+    for (User user : users) {
+      if (user.getId() != userId && user.getEmail().equals(email)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+//- deleteUser
+//- displaySchedulesByUserId
+//- searchByUserAndTitle
+//- searchByUserAndDate
+//- searchByUserAndPriority
+//- sortByUserAndDate
+//- sortByUserAndPriority
+//- sortByUserAndCompletion
+
+}
